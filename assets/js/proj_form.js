a595e32b09dc2153ba27f9c2b608ba6b346ce949
iliess <iliessabdelmadjid@gmail.com>
2024-05-07 00:44:58 +0200

    fin des task et proj formulaire ainsi que du tableau

--- assets/js/proj_form.js
+++ assets/js/proj_form.js
@@ -4,6 +4,7 @@ export function proj_form(){
 
     const buttonn_header = document.querySelector('.button_header');
     const proj_form=document.getElementById('proj_form');
+    const submit_button = document.getElementById('submit_proj');
 
     proj_form.style.display='none';
 
@@ -21,16 +22,26 @@ export function proj_form(){
 
         }
 
-        tab=[{
-             titre:'',
-             description:'',
-             date:'',
-             task:[]
-            }];
-            
-        
-
     });
 
+    //submit_proj
+    
+    const proj_tab=[];
+    submit_button.addEventListener('click',event=>{
 
+        const titre = document.getElementById('titre_proj');
+        const date = document.getElementById('date_task');
+        const description = document.getElementById('description_projet');
+        const status = document.getElementById('status_projet');
+        let table_task = task_form();
+        const item ={
+            titre:titre.value,
+            date:date.value,
+            description:description.value,
+            status:status.value,
+            task:table_task
+        }
+        proj_tab.push(item);
+        // console.log(item);
+    })
 }
--- assets/js/task_form.js
+++ assets/js/task_form.js
@@ -4,7 +4,7 @@ export function task_form(){
 
     const buttonn_header = document.querySelector('.button_header');
     const task_form=document.getElementById('task_form');
-
+    const submit_button = document.getElementById('submit_task');
 
     if(buttonn_header.id!=='add_task'){
 
@@ -24,13 +24,26 @@ export function task_form(){
 
             }
     
-            task=[{
-                titre:'',
-                date:'',
-                description:'',
-                status:''
-            }];
-    
         });
     }
+    
+    //creation du tableau qui vas contenir les differente task
+    // submit_task
+    const task=[];
+    submit_button.addEventListener('click',event=>{
+        
+        const titre = document.getElementById('titre_task');
+        const date = document.getElementById('date_task');
+        const description = document.getElementById('description_task');
+        const status = document.getElementById('status_task');
+
+        const item ={
+            titre:titre.value,
+            date:date.value,
+            description:description.value,
+            status:status.value
+        }
+        task.push(item);
+    })
+    return task
 }
