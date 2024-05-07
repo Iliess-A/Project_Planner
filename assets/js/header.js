b739e92f34028d51675c5abc092c8ae17f5dabc3
iliess <iliessabdelmadjid@gmail.com>
2024-05-07 11:36:14 +0200

    reglage bog

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,16 +1,15 @@
 export function buttonHeader(){
     const buttonHeader = document.querySelector('.button_header');
     const div = document.querySelector('.cards');
-    console.log(div.id);
-    if(div.id === 'projects'){
-        console.log('project');
-    }
-    else{
+    if(div){
+
+        if(div.id==='tasks'){
         buttonHeader.removeAttribute('id','add_projects');
         buttonHeader.setAttribute('id', 'add_task');
         buttonHeader.value = 'Add a task';
         console.log('task');
         console.log(buttonHeader);
+        }
     }
 }
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,5 @@ import{buttonHeader} from './header.js';
 buttonHeader();
 import {proj_form} from "./proj_form.js";
 proj_form();
+import { AddProject } from './cards.js';
+AddProject();
